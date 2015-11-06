16576fd70c2d70c86d5d3338e19c157e3a6d18d1
Hunter Schoonover <hunter@skoonie.com>
2015-11-06 14:52:52 -0700

    Finished TASK :: Add GUI to Reserve Material Window
    The Reserve Material window now has a Quantity input panel and a Reserve
    for combo box.

--- src/view/ReserveMaterialFrame.java
+++ src/view/ReserveMaterialFrame.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
-* Title: TransferMaterialFrame.java
+* Title: ReserveMaterialFrame.java
 * Author: Hunter Schoonover
 * Date: 07/25/15
 *
 * Purpose:
 *
-* This class is the Transfer Material window.
+* This class is the Reserve Material window.
 *
 */
 
@@ -15,17 +15,29 @@ package view;
 
 //------------------------------------------------------------------------------
 
+import java.awt.Color;
+import java.awt.Component;
+import static java.awt.Component.LEFT_ALIGNMENT;
+import static java.awt.Component.TOP_ALIGNMENT;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import mksystems.mswing.MFloatSpinner;
 import toolkit.Tools;
 
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
-// class TransferMaterialFrame
+// class ReserveMaterialFrame
 //
 
 public class ReserveMaterialFrame extends JFrame
@@ -37,7 +49,7 @@ public class ReserveMaterialFrame extends JFrame
     private GuiUpdater guiUpdater;
 
     //--------------------------------------------------------------------------
-    // TransferMaterialFrame::TransferMaterialFrame (constructor)
+    // ReserveMaterialFrame::ReserveMaterialFrame (constructor)
     //
 
     public ReserveMaterialFrame(MainView pMainView)
@@ -45,11 +57,11 @@ public class ReserveMaterialFrame extends JFrame
 
         mainView = pMainView;
 
-    }//end of TransferMaterialFrame::TransferMaterialFrame (constructor)
+    }//end of ReserveMaterialFrame::ReserveMaterialFrame (constructor)
     //--------------------------------------------------------------------------
 
     //--------------------------------------------------------------------------
-    // TransferMaterialFrame::init
+    // ReserveMaterialFrame::init
     //
     // Initializes the object. Must be called immediately after instantiation.
     //
@@ -74,11 +86,11 @@ public class ReserveMaterialFrame extends JFrame
         
         centerJFrame(this);
 
-    }// end of TransferMaterialFrame::init
+    }// end of ReserveMaterialFrame::init
     //--------------------------------------------------------------------------
     
     //--------------------------------------------------------------------------
-    // TransferMaterialFrame::centerJFrame
+    // ReserveMaterialFrame::centerJFrame
     //
     // Centers a passed in JFrame according its size and the available screen
     // size.
@@ -86,31 +98,170 @@ public class ReserveMaterialFrame extends JFrame
 
     public void centerJFrame(JFrame pFrame)
     {
-
-        int screenWidth = 
-                    java.awt.Toolkit.getDefaultToolkit().getScreenSize().width;
-        int screenHalfWidth = (int)screenWidth/2;
-
-        int screenHeight = 
-                    java.awt.Toolkit.getDefaultToolkit().getScreenSize().height;
-        int screenHalfHeight = (int)screenHeight/2;
-
-        int frameWidthCenter = (int)pFrame.getWidth()/2;
-        int frameHeightCenter = (int)pFrame.getHeight()/2;
-
-        int xPos = screenHalfWidth - frameWidthCenter;
-        int yPos  = screenHalfHeight - frameHeightCenter;
-
-        //DEBUG HSS//pFrame.setLocation(xPos, yPos);
         
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
+        this.setLocation(dim.width/2-this.getSize().width/2, 
+                            dim.height/2-this.getSize().height/2);
 
-    }// end of TransferMaterialFrame::centerJFrame
+    }// end of ReserveMaterialFrame::centerJFrame
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // ReceiveMaterialFrame::createCancelReservePanel
+    //
+    // Creates and returns the panel used to hold the Cancel and Reserve
+    // buttons.
+    //
+
+    private JPanel createCancelReservePanel()
+    {
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        panel.setAlignmentX(LEFT_ALIGNMENT);
+        panel.setAlignmentY(TOP_ALIGNMENT);
+        
+        //horizontal center 
+        //-- only works to "push" to the center if another glue is used
+        panel.add(Box.createHorizontalGlue());
+        
+        //create button
+        JButton btn = new JButton("Cancel");
+        btn.addActionListener(mainView);
+        btn.setActionCommand("ReserveMaterialFrame--Cancel");
+        btn.setAlignmentX(LEFT_ALIGNMENT);
+        btn.setFocusPainted(false);
+        btn.setHorizontalTextPosition(SwingConstants.CENTER);
+        btn.setToolTipText("Cancel.");
+        btn.setVerticalTextPosition(SwingConstants.BOTTOM);
+        Tools.setSizes(btn, 80, 30);
+        panel.add(btn);
+        
+        //horizontal spacer
+        panel.add(Box.createRigidArea(new Dimension(20,0)));
+        
+        //create button
+        JButton btn2 = new JButton("Reserve");
+        btn2.addActionListener(mainView);
+        btn2.setActionCommand("ReserveMaterialFrame--Reserve");
+        btn2.setAlignmentX(LEFT_ALIGNMENT);
+        btn2.setFocusPainted(false);
+        btn2.setHorizontalTextPosition(SwingConstants.CENTER);
+        btn2.setToolTipText("Reserve the material.");
+        btn2.setVerticalTextPosition(SwingConstants.BOTTOM);   
+        Tools.setSizes(btn2, 80, 30);
+        panel.add(btn2);
+        
+        //horizontal center 
+        //-- only works to "push" to the center if another glue is used
+        panel.add(Box.createHorizontalGlue());
+
+        return panel;
+
+    }// end of ReceiveMaterialFrame::createCancelReservePanel
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // ReserveMaterialFrame::createQuantityInputPanel
+    //
+    // Creates and returns the Quantity input panel.
+    //
+
+    private JPanel createQuantityInputPanel()
+    {
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setAlignmentX(LEFT_ALIGNMENT);
+        panel.setAlignmentY(TOP_ALIGNMENT);
+        
+        JLabel label = new JLabel("Quantity");
+        label.setAlignmentX(Component.LEFT_ALIGNMENT);
+        panel.add(label);
+        
+        //WIP HSS// -- Float spinner value and max should be set according to
+        //              the material that is being shipped
+        MFloatSpinner spinner 
+                        = new MFloatSpinner(1000, 0, 1000, 1, "##0", 75, 30);
+        //DEBUG HSS//cuttingHeadPositionSpinner.addChangeListener(this);
+        spinner.setName("ReserveMaterialFrame--Quantity Spinner");
+        spinner.setToolTipText("How many pieces of material would you like to"
+                                + " reserve?");
+        spinner.centerText();
+        spinner.setAlignmentX(Component.LEFT_ALIGNMENT);
+        panel.add(spinner);
+
+        return panel;
+
+    }// end of ReserveMaterialFrame::createQuantityInputPanel
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // ReserveMaterialFrame::creatReserveForPanel
+    //
+    // Creates and returns the "Reserve for" panel.
+    //
+
+    private JPanel createReserveForPanel()
+    {
+        
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setAlignmentX(LEFT_ALIGNMENT);
+        panel.setAlignmentY(TOP_ALIGNMENT);
+        
+        JLabel label = new JLabel("Reserve for");
+        label.setAlignmentX(Component.LEFT_ALIGNMENT);
+        label.setAlignmentY(TOP_ALIGNMENT);
+        panel.add(label);
+        
+        //populate an array of strings with items for a combo box
+        String[] strings = { "--Select--", "Shipment", "Move", "Transfer" };
+        //Create the combo box, select item at index 0
+        JComboBox combo = new JComboBox(strings);
+        combo.addActionListener(mainView);     
+        combo.setActionCommand("ReserveMaterialFrame::Reserve for");
+        combo.setAlignmentX(LEFT_ALIGNMENT);
+        combo.setAlignmentY(TOP_ALIGNMENT);
+        combo.setSelectedIndex(0);
+        combo.setBackground(Color.white);
+        Tools.setSizes(combo, 135, 30);
+        panel.add(combo);
+        
+        return panel;
+
+    }// end of ReserveMaterialFrame::createReserveForPanel
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // ReserveMaterialFrame::createRow1
+    //
+    // Creates and returns a JPanel containing the Quantity input panel.
+    //
+    
+    private JPanel createRow1() {
+        
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        panel.setAlignmentX(LEFT_ALIGNMENT);
+        panel.setAlignmentY(TOP_ALIGNMENT);
+        
+        //add Quantity input panel
+        panel.add(createQuantityInputPanel());
+        
+        //horizontal spacer
+        panel.add(Box.createRigidArea(new Dimension(20,0)));
+        
+        //add Reserve for panel
+        panel.add(createReserveForPanel());
+
+        return panel;
+        
+    }// end of ReserveMaterialFrame::createRow1
     //--------------------------------------------------------------------------
 
     //--------------------------------------------------------------------------
-    // TransferMaterialFrame::setUpFrame
+    // ReserveMaterialFrame::setUpFrame
     //
     // Sets up the JFrame by setting various options and styles.
     //
@@ -140,26 +291,45 @@ public class ReserveMaterialFrame extends JFrame
         //release the window's resources when it is closed
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
-        //add a JPanel to the frame to provide a familiar container
+         //add a JPanel to the frame to provide a familiar container
         mainPanel = new JPanel();
-        Tools.setSizes(mainPanel, 300, 90);
         setContentPane(mainPanel);
+        
+        //disable window resizing
+        setResizable(false);
 
-    }// end of TransferMaterialFrame::setUpFrame
+    }// end of ReserveMaterialFrame::setUpFrame
     //--------------------------------------------------------------------------
 
     //--------------------------------------------------------------------------
-    // TransferMaterialFrame::setupGUI
+    // ReserveMaterialFrame::setupGUI
     //
     // Sets up the user interface on the mainPanel: buttons, displays, etc.
     //
 
     private void setupGui()
     {
+        
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        
+        //add padding/margins to the main panel
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        
+        //vertical spacer
+        mainPanel.add(Box.createRigidArea(new Dimension(0,20)));
+        
+        //add Row 1
+        mainPanel.add(createRow1());
+        
+        //vertical spacer
+        mainPanel.add(Box.createRigidArea(new Dimension(0,30)));
+        
+        //add the Cancel Reserve panel
+        mainPanel.add(createCancelReservePanel());
 
-    }// end of TransferMaterialFrame::setupGui
+    }// end of ReserveMaterialFrame::setupGui
     //--------------------------------------------------------------------------
 
-}//end of class TransferMaterialFrame
+}// end of class ReserveMaterialFrame
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
