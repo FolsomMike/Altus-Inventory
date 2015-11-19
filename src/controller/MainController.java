a336b2596a0cf05b70940c82c8bed243330246d0
Hunter Schoonover <hunter@skoonie.com>
2015-11-19 12:18:16 -0600

    Program now Accesses Remote MySQL Database
    The program now accesses a remote MySQL Database and reads data from it.
    
    The MySQLDatabase class has been created to connect to and send and
    retrieve data to and from the MySQL database containing all of the data
    for the program. Currently, it has minimal functionality.
    
    The Connector/J driver necessary for accessing a MySQL database has been
    downloaded and copied into the project folder. The jar file was added to
    the project as a library. Git has been told not to ignore the jar file.
    
    Code for testing and debugging the MySQLDatabase class has been added to
    the MainController class.
    
    MainController class also now makes use of the Java Logger.

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -42,12 +42,25 @@ package controller;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.DecimalFormat;
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.TableModelEvent;
 import model.Options;
 import view.MainView;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import model.MySQLDatabase;
 
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
@@ -91,11 +104,14 @@ public class MainController implements EventHandler, Runnable
     //--------------------------------------------------------------------------
     // MainController::init
     //
-    // Initializes the object.  Must be called immediately after instantiation.
+    // Initializes the object. Must be called immediately after instantiation.
     //
 
     public void init()
     {
+        
+        //set up the logger
+        setupJavaLogger();
 
         view = new MainView(this);
         view.init();
@@ -107,6 +123,16 @@ public class MainController implements EventHandler, Runnable
         new Thread(this).start();
 
         view.setupAndStartMainTimer();
+        
+        //DEBUG HSS// -- this stuff has to be done somewhere else as needed
+        MySQLDatabase db = new MySQLDatabase();
+        db.init();
+        ArrayList<String> names = db.getCompanies();
+        
+        for (String name : names) {
+            System.out.println("Company name: " + name);
+        }
+        //DEBUG HSS//
 
     }// end of MainController::init
     //--------------------------------------------------------------------------
@@ -277,6 +303,30 @@ public class MainController implements EventHandler, Runnable
 
     }//end of MainController::control
     //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // MainController::deleteFileIfOverSizeLimit
+    //
+    // If file pFilename is larger than pLimit, the file is deleted.
+    //
+
+    private void deleteFileIfOverSizeLimit(String pFilename, int pLimit)
+    {
+
+        //delete the logging file if it has become too large
+
+        Path p1 = Paths.get(pFilename);
+
+        try { if (Files.size(p1) > pLimit){ Files.delete(p1); } }
+        catch(NoSuchFileException nsfe){
+            //do nothing if file not found -- will be recreated as needed
+        }
+        catch (IOException e) {
+            //do nothing if error on deletion -- will be deleted next time
+        }
+
+    }//end of MainController::deleteFileIfOverSizeLimit
+    //--------------------------------------------------------------------------
 
     //--------------------------------------------------------------------------
     // MainController::displayErrorMessage
@@ -380,6 +430,87 @@ public class MainController implements EventHandler, Runnable
 
     }//end of MainController::doTimerActions
     //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // MainController::logSevere
+    //
+    // Logs pMessage with level SEVERE using the Java logger.
+    //
+
+    void logSevere(String pMessage)
+    {
+
+        Logger.getLogger(getClass().getName()).log(Level.SEVERE, pMessage);
+
+    }//end of MainController::logSevere
+    //--------------------------------------------------------------------------
+
+    //--------------------------------------------------------------------------
+    // MainController::logStackTrace
+    //
+    // Logs stack trace info for exception pE with pMessage at level SEVERE 
+    // using the Java logger.
+    //
+
+    void logStackTrace(String pMessage, Exception pE)
+    {
+
+        Logger.getLogger(getClass().getName()).log(Level.SEVERE, pMessage, pE);
+
+    }//end of MainController::logStackTrace
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // MainController::setupJavaLogger
+    //
+    // Prepares the Java logging system for use. Output is directed to a file.
+    //
+    // Each time the method is called, it checks to see if the file is larger
+    // than the maximum allowable size and deletes the file if so.
+    //
+
+    private void setupJavaLogger()
+    {
+
+        String logFilename = "Java Logger File.txt";
+
+        //prevent the logging file from getting too big
+        deleteFileIfOverSizeLimit(logFilename, 10000);
+
+        //remove all existing handlers from the root logger (and thus all child
+        //loggers) so the output is not sent to the console
+
+        Logger rootLogger = Logger.getLogger("");
+        Handler[] handlers = rootLogger.getHandlers();
+        for(Handler handler : handlers) {
+            rootLogger.removeHandler(handler);
+        }
+
+        //add a new handler to send the output to a file
+
+        Handler fh;
+
+        try {
+
+            //write log to logFilename, 10000 byte limit on each file, rotate
+            //between two files, append the the current file each startup
+
+            fh = new FileHandler(logFilename, 10000, 2, true);
+
+            //direct output to a file for the root logger and  all child loggers
+            Logger.getLogger("").addHandler(fh);
+
+            //use simple text output rather than default XML format
+            fh.setFormatter(new SimpleFormatter());
+
+            //record all log messages
+            Logger.getLogger("").setLevel(Level.WARNING);
+
+        }
+        catch(IOException e){ }
+
+    }//end of MainController::setupJavaLogger
+    //--------------------------------------------------------------------------
 
     //--------------------------------------------------------------------------
     // MainController::shutDown
@@ -443,3 +574,12 @@ public class MainController implements EventHandler, Runnable
 }//end of class MainController
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
+
+//DEBUG HSS// -- this class is only temporary and should be removed later
+class Parameters {
+
+    ResultSet resultSet;
+    
+    public Parameters() {}
+        
+}
---
+++ src/model/MySQLDatabase.java
@@ -0,0 +1,362 @@
+/*******************************************************************************
+* Title: MySQLDatabase.java
+* Author: Hunter Schoonover
+* Date: 11/18/15
+*
+* Purpose:
+*
+* This class is used to connect to and send and retrieve data to and from the 
+* MySQL database containing all of the data for the program.
+* 
+* NOTES:
+*   Since opening a connection can be an expensive process for the server, it is
+*   best to minimize the number of times you close and reopen the connection. 
+*   This problem should not be solved by keeping a connection open for all of 
+*   eternity; this uses valuable resources and can negatively affect the 
+*   application's overall performance. Instead, if you have a series of commands
+*   that you are sending to the database one after another, then open the 
+*   connection once by calling connectToDatabase() and close it by calling 
+*   closeDatabaseConnection() after all of the commands have been sent. A long 
+*   wait time between command calls should be handled by opening and closing the
+*   connection between each series of calls.
+* 
+* In order to use this class, the latest version of Connector/J must be added to
+* to the project. In order to add Connector/J to your project in NetBeans, 
+* follow these steps:
+* 1. Download the latest version of Connector/J from 
+*       http://dev.mysql.com/downloads/connector/j/
+* 
+* 2. After downloading, unzip the folder to a general location that is not
+*       specific to your project; the driver can be used for future projects.
+* 
+* 3. Copy the jar file from the unzipped folder to your Netbeans project folder.
+*       The jar file will have look similar to 
+*       mysql-connector-java-5.1.37-bin.jar but will vary depending on the
+*       latest version.
+* 
+* 4. Launch Netbeans and expand your project in the Projects pane to the left.
+*
+* 5. Right-click on Libraries and click "Add Jar/Folder".
+* 
+* 6. Select the jar file that you just copied into your project folder.
+* 
+* 7. Before using the driver, you have to register it: 
+*       Class.forName("com.mysql.jdbc.Driver");
+* 
+*       For more information on why you have to register the driver:
+*           http://www.xyzws.com/Javafaq/what-does-classforname-method-do/17
+*
+* Open Source Policy:
+*
+* This source code is Public Domain and free to any interested party.  Any
+* person, company, or organization may do with it as they please.
+*
+*/
+
+//------------------------------------------------------------------------------
+
+package model;
+
+//------------------------------------------------------------------------------
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+// class MySQLDatabase
+//
+
+public class MySQLDatabase 
+{
+    
+    private final String url = "jdbc:mysql://108.167.140.102:3306/"
+                                    + "hunter98_altus-inventory";
+    private final String username = "hunter98_altus";
+    private final String password = "shyanismineagain98";
+    
+    private Connection connection;
+    
+    //--------------------------------------------------------------------------
+    // MySQLDatabase::MySQLDatabase (constructor)
+    //
+
+    public MySQLDatabase()
+    {
+
+    }//end of MySQLDatabase::MySQLDatabase (constructor)
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // MySQLDatabase::init
+    //
+    // Initializes the object. Must be called immediately after instantiation.
+    //
+
+    public void init()
+    {
+        
+        registerJDBCDriver();
+
+    }//end of MySQLDatabase::init
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // MySQLDatabase::closeDatabaseConnection
+    //
+    // Closes the connection to the database.
+    //
+    // This should be called after sending commands to the database to improve 
+    // code performance and free up resources.
+    // Read the NOTES at the top of the file for more information on when to
+    // connect and when to close the connection.
+    //
+
+    private void closeDatabaseConnection()
+    {
+        
+        try { 
+            //return if the connection is not open
+            if (connection == null || connection.isClosed()) { return; }
+            
+            connection.close();
+        }
+        catch (SQLException e) { logSevere(e.getMessage() + " - Error: 129"); }
+        
+    }//end of MySQLDatabase::closeDatabaseConnection
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // MySQLDatabase::closeResultSet
+    //
+    // Closes the passed in ResultSet to free up resources.
+    //
+
+    private void closeResultSet(ResultSet pSet)
+    {
+        
+        try { if (pSet != null) { pSet.close(); } }
+        catch (SQLException e) { logSevere(e.getMessage() + " - Error: 144"); }
+
+    }//end of MySQLDatabase::closeResultSet
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // MySQLDatabase::closePreparedStatement
+    //
+    // Closes the passed in PreparedStatement to free up resources.
+    //
+
+    private void closePreparedStatement(PreparedStatement pStatement)
+    {
+        
+        try { if (pStatement != null) { pStatement.close(); } }
+        catch (SQLException e) { logSevere(e.getMessage() + " - Error: 161"); }
+
+    }//end of MySQLDatabase::closePreparedStatement
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // MySQLDatabase::connectToDatabase
+    //
+    // Establishes a connection with the database if one is not already
+    // established.
+    //
+    // Returns true upon success; false upon failure.
+    //
+    // This should be called before sending any commands to the database. 
+    // Read the NOTES at the top of the file for more information on when to
+    // connect and when to close the connection.
+    //
+
+    private boolean connectToDatabase()
+    {
+        
+        boolean success = true;
+        
+        try { 
+            //return true if we are already connected
+            if(connection != null && !connection.isClosed()) { return success; }
+            
+            connection = DriverManager.getConnection(url, username, password);
+        }
+        catch (SQLException e) { 
+            success = false;
+            logSevere(e.getMessage() + " - Error: 191"); 
+        }
+        
+        return success;
+        
+    }//end of MySQLDatabase::connectToDatabase
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // MySQLDatabase::createPreparedStatement
+    //
+    // Creates a PreparedStatment using the passed in SQL command string.
+    // Returns PreparedStatment upon success; null upon failure.
+    //
+
+    private PreparedStatement createPreparedStatement(String pCommand)
+    {
+        
+        PreparedStatement stmt = null;
+        
+        //ensure that we are connected to the database -- return null if a 
+        //conection can't be established
+        if (!connectToDatabase()) { return stmt; }
+        
+        try { stmt = connection.prepareStatement(pCommand); }
+        catch (SQLException e) { logSevere(e.getMessage() + " - Error: 216"); }
+        
+        return stmt;
+
+    }//end of MySQLDatabase::createPreparedStatement
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // MySQLDatabase::getCompanies
+    //
+    // Gets and returns the compainies from the database.
+    //
+    // //WIP HSS// -- this needs to be changed epically; right now it only 
+    //                  returns company names
+    //
+
+    public ArrayList<String> getCompanies()
+    {
+        
+        ArrayList<String> names = new ArrayList();
+
+        String cmd = "SELECT `display_name` FROM `Customers`"
+                        + " ORDER BY `display_name` ASC";
+        PreparedStatement stmt = createPreparedStatement(cmd);
+        ResultSet set = performQuery(stmt);
+        
+        try { while (set.next()) { names.add(set.getString("display_name")); } }
+        catch (SQLException e) { logSevere(e.getMessage() + " - Error: 242"); }
+        
+        //clean up environment
+        closeResultSet(set);
+        closePreparedStatement(stmt);
+        closeDatabaseConnection();
+        
+        return names;
+
+    }//end of MySQLDatabase::getCompanies
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // MySQLDatabase::getNumberOfBatchesInYard
+    //
+    // Gets and returns the number of batches in the yard by counting all of 
+    // the batches in the Batches table with a quantity greater than 0.
+    //
+
+    public int getNumberOfBatchesInYard()
+    {
+        
+        int batches = 0;
+        
+        try { 
+            String cmd = "SELECT COUNT(*) FROM BATCHES WHERE QUANTITY > 0";
+            PreparedStatement stmt = createPreparedStatement(cmd);
+            ResultSet set = performQuery(stmt);
+            while (set.next()) { batches = set.getInt(1); } 
+            
+            //clean up environment
+            closeResultSet(set);
+            closePreparedStatement(stmt);
+            closeDatabaseConnection();
+        }
+        catch (SQLException e) { logSevere(e.getMessage() + " - Error: 277"); }
+        
+        return batches;
+
+    }//end of MySQLDatabase::getNumberOfBatchesInYard
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // MySQLDatabase::logSevere
+    //
+    // Logs pMessage with level SEVERE using the Java logger.
+    //
+
+    private void logSevere(String pMessage)
+    {
+
+        Logger.getLogger(getClass().getName()).log(Level.SEVERE, pMessage);
+
+    }//end of MySQLDatabase::logSevere
+    //--------------------------------------------------------------------------
+
+    //--------------------------------------------------------------------------
+    // MySQLDatabase::logStackTrace
+    //
+    // Logs stack trace info for exception pE with pMessage at level SEVERE 
+    // using the Java logger.
+    //
+
+    private void logStackTrace(String pMessage, Exception pE)
+    {
+
+        Logger.getLogger(getClass().getName()).log(Level.SEVERE, pMessage, pE);
+
+    }//end of MySQLDatabase::logStackTrace
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // MySQLDatabase::registerJDBCDriver
+    //
+    // Loads and registers the JDBC driver.
+    //
+
+    private void registerJDBCDriver()
+    {
+        
+        //Register JDBC driver
+        try { Class.forName("com.mysql.jdbc.Driver"); }
+        catch (ClassNotFoundException e) { 
+            logSevere(e.getMessage() + " - Error: 325"); 
+        }
+
+    }//end of MySQLDatabase::registerJDBCDriver
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // MySQLDatabase::performQuery
+    //
+    // Performs a query on the database using the passed in PreparedStatment. 
+    // Returns ResultSet upon success; null upon failure.
+    //
+    // NOTE:    To improve code performance and free up resources, the ResultSet 
+    //          returned should be closed after use by using the 
+    //          closeResultSet() helper function provided by this class.
+    //
+
+    private ResultSet performQuery(PreparedStatement pStatement)
+    {
+        
+        ResultSet set = null;
+        
+        //ensure that we are connected to the database -- return null if a 
+        //conection can't be established
+        if (!connectToDatabase()) { return set; }
+        
+        try { set = pStatement.executeQuery(); }
+        catch (SQLException e) { logSevere(e.getMessage() + " - Error: 352"); }
+        
+        return set;
+
+    }//end of MySQLDatabase::performQuery
+    //--------------------------------------------------------------------------
+    
+}//end of class MySQLDatabase
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
