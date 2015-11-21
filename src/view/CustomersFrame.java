9fce22621b77ef017c800c603b60e7443bc77fa3
Hunter Schoonover <hunter@skoonie.com>
2015-11-20 21:07:55 -0600

    Customers window retrieves customers from database and displays them.
    The CustomersFrame class now retrieves the customers from the database on
    HostGator and displays their ids and names in a CustomTable.
    
    Code in CustomersFrame class needs to be cleaned up.
    
    Signed-off-by: Hunter Schoonover <hunter@skoonie.com>

--- src/view/CustomersFrame.java
+++ src/view/CustomersFrame.java
@@ -16,16 +16,33 @@ package view;
 
 //------------------------------------------------------------------------------
 
+import java.awt.Color;
+import static java.awt.Component.LEFT_ALIGNMENT;
+import static java.awt.Component.TOP_ALIGNMENT;
+import java.awt.Font;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.TableColumnModel;
+import model.Customer;
+import model.MySQLDatabase;
 import skooniecomponents.frame.SkoonieFrame;
+import toolkit.Tools;
 
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 // class  CustomersFrame
 //
 
-public class  CustomersFrame extends SkoonieFrame
+public class CustomersFrame extends SkoonieFrame
 {
+    
+    //declared as object so that they can be easily added to the table
+    private List<String> customerIds = new ArrayList<>();
+    private List<String> customerNames = new ArrayList<>();
 
     //--------------------------------------------------------------------------
     // CustomersFrame::CustomersFrame (constructor)
@@ -49,6 +66,23 @@ public class  CustomersFrame extends SkoonieFrame
     //-------------------------------------------------------------------------
     
     //--------------------------------------------------------------------------
+    // CustomersFrame::init
+    //
+    // Initializes the object. Must be called immediately after instantiation.
+    //
+    
+    @Override
+    public void init() 
+    {
+        
+        getCustomerInfoFromDatabase();
+        
+        super.init();
+        
+    }// end of CustomersFrame::init
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
     // CustomersFrame::createGui
     //
     // Creates and adds the GUI to the mainPanel.
@@ -60,10 +94,61 @@ public class  CustomersFrame extends SkoonieFrame
         
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
         
-        //vertical spacer
-        mainPanel.add(createVerticalSpacer(20));
+        String[] columnNames = {"Id", "Customer"};
+        
+        String[][] data = new String[customerIds.size()][];
+        
+        for (int i=0; i<data.length; i++) {
+            data[i] = new String[]{customerIds.get(i), customerNames.get(i)};
+        }
+        
+        CustomTable table = new CustomTable(data, columnNames);
+        
+        //setup the table
+        table.getTableHeader().setBackground(Color.decode("#C2E0FF"));
+        table.getTableHeader().setFont(new Font("Times Roman", Font.BOLD, 15));
+        table.getTableHeader().setReorderingAllowed(false);
+        table.setRowHeight(25);
+        table.setSelectionBackground(Color.decode("#000099"));
+        table.setSelectionForeground(Color.WHITE);
+        table.setBorder(BorderFactory.createEmptyBorder());
+        
+        //set the widths of the columns
+        TableColumnModel m =  table.getColumnModel();
+        m.getColumn(0).setPreferredWidth(100);
+        m.getColumn(1).setPreferredWidth(300);
+        
+        //put the table in a scroll pane
+        JScrollPane sp = new JScrollPane(table);
+        sp.setAlignmentX(LEFT_ALIGNMENT);
+        sp.setAlignmentY(TOP_ALIGNMENT);
+        //sp.setBorder(BorderFactory.createEmptyBorder());
+        Tools.setSizes(sp, 400, 300);
+        mainPanel.add(sp);
         
     }// end of CustomersFrame::createGui
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // CustomersFrame::getCustomerInfoFromDatabase
+    //
+    // Retrieves the customer info from the MySQL database and stores the
+    // ids and names.
+    //
+    
+    private void getCustomerInfoFromDatabase() 
+    {
+        
+        MySQLDatabase db = new MySQLDatabase();
+        db.init();
+        
+        ArrayList<Customer> customers = db.getCustomers();
+        for (Customer c : customers) {
+            customerIds.add(c.getId());
+            customerNames.add(c.getDisplayName());
+        }
+        
+    }// end of CustomersFrame::getCustomerInfoFromDatabase
     //--------------------------------------------------------------------------
 
 }//end of class  CustomersFrame
