7c1bea24bddf5cadbeca773cf8cdb3bd217f2297
Hunter Schoonover <hunter@skoonie.com>
2015-12-04 11:35:40 -0600

    MaterialInfoWindow now checks user input for errors
    Signed-off-by: Hunter Schoonover <hunter@skoonie.com>

--- src/view/MaterialInfoWindow.java
+++ src/view/MaterialInfoWindow.java
@@ -119,6 +119,9 @@ public class MaterialInfoWindow extends AltusJDialog
     public void confirm()
     {
         
+        //check user input for errors
+        if (!checkUserInput()) { return; }
+        
         //get the user input
         getUserInput();
         
@@ -133,6 +136,65 @@ public class MaterialInfoWindow extends AltusJDialog
         dispose();
 
     }// end of MaterialInfoWindow::confirm
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // MaterialInfoWindow::checkUserInput
+    //
+    // Checks the user input for errors.
+    //
+    // Returns true if no errors; false if there are.
+    //
+
+    private boolean checkUserInput()
+    {
+        
+        //Check Id input
+        if (getIdInput().isEmpty()) {
+            displayError("Please give the material an Id.");
+            return false;
+        }
+        else if (!getIdInput().equals(batch.getId())
+                && getDatabase().checkForValue(getIdInput(), "BATCHES", "id")) 
+        {
+            displayError("The Id entered already exists in the database.");
+            return false;
+        }
+        
+        //Check Date input
+        if (getDateInput().isEmpty()) {
+            displayError("Please specify a date.");
+            return false;
+        }
+        
+        //Check Customer input
+        if (!checkCustomerInput()) { 
+            displayError("Please select a customer.");
+            return false;
+        }
+        
+        //Check Quantity input
+        if (getQuantityInput().isEmpty()) {
+            displayError("Please enter a Quantity.");
+            return false;
+        }
+        
+        //Check Total Length input
+        if (getTotalLengthInput().isEmpty()) {
+            displayError("Please enter a Total Length.");
+            return false;
+        }
+        
+        //Check Rack input
+        if (!checkRackInput()) { 
+            displayError("Please select a rack.");
+            return false;
+        }
+        
+        //we made it here, so there were no errors
+        return true;
+
+    }// end of MaterialInfoWindow::checkUserInput
     //--------------------------------------------------------------------------
     
     //--------------------------------------------------------------------------
