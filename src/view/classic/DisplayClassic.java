3681a46e822fa5053b419879d2911a6b5cb7d9d9
Hunter Schoonover <hunter@skoonie.com>
2015-12-11 17:17:37 -0600

    Added the view.classic package to the project
    Currently only contains DisplayClassic class
    
    Signed-off-by: Hunter Schoonover <hunter@skoonie.com>

---
+++ src/view/classic/DisplayClassic.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+* Title: DisplayClassic.java
+* Author: Hunter Schoonover
+* Date: 12/11/15
+*
+* Purpose:
+*
+* This class is the Classic display version. It is the GUI that will be used for
+* the original version of the program.
+*
+*/
+
+//------------------------------------------------------------------------------
+
+package view.classic;
+
+//------------------------------------------------------------------------------
+
+import command.Command;
+import command.CommandHandler;
+import command.CommandListener;
+import java.util.Scanner;
+import javax.swing.JFrame;
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+// class DisplayClassic
+//
+
+public class DisplayClassic extends JFrame implements CommandListener
+{
+
+    //--------------------------------------------------------------------------
+    // DisplayClassic::DisplayClassic (constructor)
+    //
+
+    public DisplayClassic()
+    {
+
+    }//end of DisplayClassic::DisplayClassic (constructor)
+    //--------------------------------------------------------------------------
+
+    //--------------------------------------------------------------------------
+    // DisplayClassic::init
+    //
+    // Initializes the object. Must be called immediately after instantiation.
+    //
+
+    public void init()
+    {
+        
+        //register this as a view listener
+        CommandHandler.registerViewListener(this);
+
+    }// end of DisplayClassic::init
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // DisplayClassic::commandPerformed
+    //
+    // Performs different actions depending on pCommand.
+    //
+    // The function will do nothing if pCommand was not intended for view.
+    //
+    // Called by the CommandHandler everytime a view command is performed.
+    //
+
+    @Override
+    public void commandPerformed(String pCommand)
+    {
+        
+        //return if this is not a view command
+        if(!Command.isViewCommand(pCommand)) { return; }
+
+    }//end of DisplayClassic::commandPerformed
+    //--------------------------------------------------------------------------
+
+}//end of class DisplayClassic
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
