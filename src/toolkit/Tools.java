7d46f0ffbe43da2ba4b799f32c28566a52b4ef53
Hunter Schoonover <hunter@skoonie.com>
2015-12-11 23:51:36 -0600

    Changed centerJDialog() function to accept Window for parent parameter
    Now there does not have to be two centerJDialog() functions for JDialog and
    JFrame -- Window encases both.
    
    Signed-off-by: Hunter Schoonover <hunter@skoonie.com>

--- src/toolkit/Tools.java
+++ src/toolkit/Tools.java
@@ -20,6 +20,7 @@ package toolkit;
 
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Window;
 import javax.swing.Box;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
@@ -106,7 +107,7 @@ public class Tools extends Object{
     // passed in parent dialog and the JDialog's size.
     //
 
-    static public void centerJDialog(JDialog pDialog, JDialog pParent)
+    static public void centerJDialog(JDialog pDialog, Window pParent)
     {
 
         int parentFrameXPos = (int)pParent.getX();
@@ -114,36 +115,6 @@ public class Tools extends Object{
 
         int parentFrameYPos = (int)pParent.getY();
         int parentFrameHalfHeight = (int)pParent.getHeight()/2;
-
-        int parentFrameXCenter = parentFrameXPos + parentFrameHalfWidth;
-        int parentFrameYCenter = parentFrameYPos + parentFrameHalfHeight;
-
-        int dialogWidthCenter = (int)pDialog.getWidth()/2;
-        int dialogHeightCenter = (int)pDialog.getHeight()/2;
-
-        int xPosition = parentFrameXCenter - dialogWidthCenter;
-        int yPosition = parentFrameYCenter - dialogHeightCenter;
-
-        pDialog.setLocation(xPosition, yPosition);
-
-    }// end of Tools::centerJDialog
-    //--------------------------------------------------------------------------
-    
-    //--------------------------------------------------------------------------
-    // Tools::centerJDialog
-    //
-    // Centers a passed in JDialog according to the location and size of the 
-    // passed in parent frame and the JDialog's size.
-    //
-
-    static public void centerJDialog(JDialog pDialog, JFrame pParentFrame)
-    {
-
-        int parentFrameXPos = (int)pParentFrame.getX();
-        int parentFrameHalfWidth = (int)pParentFrame.getWidth()/2;
-
-        int parentFrameYPos = (int)pParentFrame.getY();
-        int parentFrameHalfHeight = (int)pParentFrame.getHeight()/2;
 
         int parentFrameXCenter = parentFrameXPos + parentFrameHalfWidth;
         int parentFrameYCenter = parentFrameYPos + parentFrameHalfHeight;
