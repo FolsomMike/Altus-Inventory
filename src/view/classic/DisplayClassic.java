2d45329918e9856548a73aea8a135cfb2c93444e
Hunter Schoonover <hunter@skoonie.com>
2015-12-11 18:12:47 -0600

    DisplayClassic now displays control panel in main window
    Functions added to DisplayClassic:
    	createControlPanel() -- called from createGui()
    	createControlPanelButton() -- called from createControlPanel()
    	createImagIcon() -- called from createControlPanelButton()
    
    Control panel buttons currently have no functionality.
    
    Signed-off-by: Hunter Schoonover <hunter@skoonie.com>

--- src/view/classic/DisplayClassic.java
+++ src/view/classic/DisplayClassic.java
@@ -19,14 +19,22 @@ package view.classic;
 import command.Command;
 import command.CommandHandler;
 import command.CommandListener;
+import static java.awt.Component.LEFT_ALIGNMENT;
+import static java.awt.Component.TOP_ALIGNMENT;
 import static java.awt.Frame.MAXIMIZED_BOTH;
+import java.awt.Insets;
 import java.util.Map;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import toolkit.Tools;
+import view.MainView;
 
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
@@ -100,6 +108,130 @@ public class DisplayClassic extends JFrame implements CommandListener
     //--------------------------------------------------------------------------
     
     //--------------------------------------------------------------------------
+    // DisplayClassic::createControlPanel
+    //
+    // Creates and returns the control panel.
+    //
+    // The control panel contains all of the main button and functions for the 
+    // program.
+    //
+
+    private JPanel createControlPanel()
+    {
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        panel.setAlignmentX(LEFT_ALIGNMENT);
+        panel.setAlignmentY(TOP_ALIGNMENT);
+        
+        int spacer = 10;
+
+        //add Receive Material button
+        panel.add(createControlPanelButton("Receive<br>Material", 
+                                            "images/receiveMaterial.png",
+                                            "Receive material into the yard."));
+        
+        //horizontal spacer
+        panel.add(Tools.createHorizontalSpacer(spacer));
+        
+        //add Ship Material button
+        panel.add(createControlPanelButton("Ship<br>Material", 
+                                            "images/shipMaterial.png",
+                                            "Ship material from the yard."));
+        
+        //horizontal spacer
+        panel.add(Tools.createHorizontalSpacer(spacer));
+        
+        //add Move Material button
+        panel.add(createControlPanelButton("Move<br>Material", 
+                                            "images/moveMaterial.png",
+                                            "Move the selected material to a "
+                                                + "different rack."));
+        
+        //horizontal spacer
+        panel.add(Tools.createHorizontalSpacer(spacer));
+        
+        //add Transfer Material button
+        panel.add(createControlPanelButton("Transfer<br>Material", 
+                                            "images/transferMaterial.png",
+                                            "Transfer the selected material to "
+                                                + "a different customer."));
+        
+        //horizontal spacer
+        panel.add(Tools.createHorizontalSpacer(spacer));
+        
+        //add Reserve Material button
+        panel.add(createControlPanelButton("Reserve<br>Material", 
+                                            "images/reserveMaterial.png",
+                                            "Reserve the selected material for "
+                                                + "future use."));
+        
+        //horizontal spacer
+        panel.add(Tools.createHorizontalSpacer(spacer));
+        
+        //add Material Info button
+        panel.add(createControlPanelButton("Material<br>Info", 
+                                            "images/materialInfo.png",
+                                            "View and edit information about "
+                                                + "the selected material."));
+        
+        //horizontal spacer
+        panel.add(Tools.createHorizontalSpacer(spacer));
+
+        //add Create Report button
+        panel.add(createControlPanelButton("Create<br>Report", 
+                                            "images/createReport.png",
+                                            "Create a report."));
+
+        //horizontal spacer
+        panel.add(Tools.createHorizontalSpacer(spacer));
+
+        //add Create Invoice button
+        panel.add(createControlPanelButton("Create<br>Invoice", 
+                                            "images/createInvoice.png",
+                                            "Create an invoice."));
+
+        //horizontal spacer
+        panel.add(Tools.createHorizontalSpacer(spacer));
+
+        //add Make Payment button
+        panel.add(createControlPanelButton("Make<br>Payment", 
+                                            "images/makePayment.png",
+                                            "Record payment from a customer."));
+
+        return panel;
+
+    }// end of DisplayClassic::createControlPanel
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // DisplayClassic::createControlPanelButton
+    //
+    // Creates and returns a button to be used in the control panel part of the
+    // main window, using the parameters for individuality.
+    //
+
+    private JButton createControlPanelButton(String pText, String pImagePath,
+                                                String pTip)
+    {
+
+        //create button
+        JButton btn = new JButton("<html><center>" + pText + "</center></html>", 
+                                        createImageIcon(pImagePath));
+        btn.setAlignmentX(LEFT_ALIGNMENT);
+        btn.setFocusPainted(false);
+        btn.setHorizontalTextPosition(SwingConstants.CENTER);
+        btn.setMargin(new Insets(0,0,0,0));
+        btn.setToolTipText(pTip);
+        btn.setVerticalTextPosition(SwingConstants.BOTTOM);        
+        Tools.setSizes(btn, 70, 75);
+        
+        return btn;
+
+    }// end of DisplayClassic::createControlPanelButton
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
     // DisplayClassic::createGui
     //
     // Creates and adds the GUI to the window.
@@ -113,8 +245,36 @@ public class DisplayClassic extends JFrame implements CommandListener
         
         //set the layout to add items top to bottom
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        
+        //add the control panel to the gui
+        mainPanel.add(createControlPanel());
 
     }// end of DisplayClassic::createGui
+    //--------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------
+    // DisplayClassic::createImageIcon
+    //
+    // Returns an ImageIcon, or null if the path was invalid.
+    //
+    // *************************************************************************
+    // NOTE: You must use forward slashes in the path names for the resource
+    // loader to find the image files in the JAR package.
+    // *************************************************************************
+    //
+
+    private ImageIcon createImageIcon(String pPath)
+    {
+
+        // have to use the MainView class because it is located in the same 
+        // package as the file; specifying the class specifies the first 
+        // portion of the path to the image, this concatenated with the pPath
+        java.net.URL imgURL = MainView.class.getResource(pPath);
+
+        if (imgURL != null) { return new ImageIcon(imgURL); }
+        else {return null;}
+
+    }//end of DisplayClassic::createImageIcon
     //--------------------------------------------------------------------------
     
     //--------------------------------------------------------------------------
